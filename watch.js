894a27819204b0269991923ea1a8c30929483bdb
Jbone187 <jasen.green@gmail.com>
2017-10-26 14:25:32 -0700

    Added program

---
+++ watch.js
@@ -0,0 +1,66 @@
+var Not = require('./notify');
+fsmonitor = require('fsmonitor');
+var winston = require('./winston');
+var fs = require('fs.extra');
+
+fsmonitor.watch('\wtest', null, function(change) {
+
+       if (typeof change === "object") {
+
+    console.log("Change detected:\n" + change); 
+    winston.winston.log('info', "Change detected:\n" + change);
+    winston.winston.log('info', "Added files:   %j", change.addedFiles);
+    console.log("Added files:   %j", change.addedFiles);
+    console.log("Modified files: %j", change.modifiedFiles);
+    console.log("Removed files:  %j", change.removedFiles);
+
+    console.log("Added folders:    %j", change.addedFolders);
+    console.log("Modified folders: %j", change.modifiedFolders);
+    console.log("Removed folders:  %j", change.removedFolders);
+
+//Run Associated Modules
+    Not();
+ 
+   
+   
+   
+    //Copy file Code
+    fs.copyRecursive('/nodetest/wtest', '/nodetest/rfile2', function (err) {
+        if (err) {
+          throw err;
+        }
+       
+        console.log("Copied");
+      });
+
+  
+  
+  
+  
+  
+  
+    } else {
+         console.log("Error");
+    };
+});
+
+
+
+
+var monitor = fsmonitor.watch('.', {
+    // include files
+    matches: function(relpath) {
+        return relpath.match(/\.txt$/i) !== null;
+    },
+    // exclude directories
+    excludes: function(relpath) {
+        return relpath.match(/^\.git$/i) !== null;
+    }
+});
+
+monitor.on('change', function(changes) {
+    console.log(changes);
+});
+
+
+
