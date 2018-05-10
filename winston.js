908bf023947fb6134528852b54070ab0c801e173
Jasen Green <jasen.green@protonmail.com>
2018-05-10 14:12:57 -0700

    Update winston.js

--- winston.js
+++ winston.js
@@ -10,9 +10,6 @@ winston.add(
     eol: 'rn', // for Windows, or `eol: ‘n’,` for *NIX OSs
     timestamp: true
   }
-)
-
-//winston.log('info', 'Hello log files!')
-//winston.info('Hello again log files!')
+);
 
 module.exports.winston = winston;
