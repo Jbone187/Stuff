b62981d8311ecd746f67ed3d3ffa58f65180c76d
Jasen Green <jasen.green@protonmail.com>
2018-05-10 14:14:15 -0700

    Update notify.js

--- notify.js
+++ notify.js
@@ -1,21 +1,7 @@
 module.exports = function () {
 
   const notifier = require('node-notifier');
-
-  // String
+ // String
   notifier.notify('Folder Was Updated');
-
-  /*  Can add Object
-  notifier.notify({
-    'title': 'Folder Watch',
-    'subtitle': 'Daily Maintenance',
-    'message': 'You need to do better',
-    'icon': 'dwb-logo.png',
-    'contentImage': 'blog.png',
-    'sound': 'ding.mp3',
-    'wait': true
-  });
-*/ 
-
 };
 
